acfb342f800d76ca7e82783981bac6c0e2b858ef
Alex Blomarz <a.blomarz@gmail.com>
2025-03-20 20:59:27 -0500

    complete react-hooks-practice-sushi-saga

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([]);
+  const [eatenSushis, setEatenSushis] = useState([]);
+  const [money, setMoney] = useState(100);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/sushis")
+      .then((r) => r.json())
+      .then((sushis) => setSushis(sushis));
+  }, []);
+
+  function handleEatSushi(sushi) {
+    if (money >= sushi.price) {
+      setEatenSushis([...eatenSushis, sushi.id]);
+      setMoney(money - sushi.price);
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer
+        sushis={sushis}
+        eatenSushis={eatenSushis}
+        onEatSushi={handleEatSushi} />
+      <Table plates={eatenSushis} money={money} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, eatenSushis, onEatSushi }) {
+  const [index, setIndex] = useState(0);
+
+  const visibleSushis = sushis.slice(index, index + 4);
+
+  function handleMoreClick() {
+    setIndex((prevIndex) => (prevIndex + 4));
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {visibleSushis.map((sushi) => (
+        <Sushi 
+          key={sushi.id}
+          sushi={sushi}
+          eaten={eatenSushis.includes(sushi.id)}
+          onEatSushi={onEatSushi}
+           />
+      ))}
+      <MoreButton onClick={handleMoreClick} />
     </div>
   );
 }
